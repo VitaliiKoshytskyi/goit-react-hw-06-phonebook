fa5d6b0d545c69294019c7e362729467b3e200ad
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-03-06 18:51:38 +0200

    updated markup

--- src/redux/filter/filter-slice.js
+++ src/redux/filter/filter-slice.js
@@ -1,13 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const filterSlice = createSlice({
-    name: "filter",
-    initialState: "",
-    reducers: {
-        setFilter: (_, {payload}) => payload,
-    }
-})
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter: (_, { payload }) => payload,
+  },
+});
 
-export const {setFilter} = filterSlice.actions;
+export const { setFilter } = filterSlice.actions;
 
 export default filterSlice.reducer;
